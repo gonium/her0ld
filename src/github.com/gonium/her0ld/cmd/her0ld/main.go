dd908de8a4cdf81221e488f8c97bd77b7f10a4ed
Mathias Dalheimer <md@gonium.net>
2016-04-25 16:26:21 +0200

    code template present - no real functionality yet

---
+++ src/github.com/gonium/her0ld/cmd/her0ld/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "her0ld"
+	app.Usage = "A friendly IRC bot written in Go."
+	app.Version = "0.1.0"
+	app.HideVersion = true
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "verbose, v",
+			Usage: "print verbose messages",
+		},
+	}
+	app.Action = func(c *cli.Context) {
+		if c.Bool("verbose") {
+			log.Println("Verbose logging active")
+		}
+		log.Fatal("Started")
+	}
+	app.Run(os.Args)
+}
