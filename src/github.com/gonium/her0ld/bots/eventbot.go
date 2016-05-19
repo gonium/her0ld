e289d4707dfc68ffd72da5486cb05d82d0f2711d
Mathias Dalheimer <md@gonium.net>
2016-05-19 14:51:31 +0200

    added skeleton of eventbot - help command implemented, testsuite up and running.

---
+++ src/github.com/gonium/her0ld/bots/eventbot.go
@@ -0,0 +1,67 @@
+package her0ldbot
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	EVENTBOT_INVALID_COMMAND = "invalid command - see !event help."
+	EVENTBOT_PREFIX          = "!event"
+	EVENTBOT_CMD_HELP        = "help"
+	EVENTBOT_HELP_TEXT       = `!event usage:
+	* help - this text
+	* add <date> <description> - add an event
+	* list - list all events (with id)
+	* del <id> - remove the event with the given id
+	* today - show all events today
+	`
+)
+
+/* The EventBot maintains a list of events and attempts to
+ * remind people. */
+type EventBot struct {
+	BotName            string
+	NumMessagesHandled int
+}
+
+func NewEventBot(name string) *EventBot {
+	return &EventBot{BotName: name, NumMessagesHandled: 0}
+}
+
+func (b *EventBot) strings2reply(dest string, lines []string) []OutboundMessage {
+	reply := make([]OutboundMessage, len(lines))
+	for idx, line := range lines {
+		reply[idx] = OutboundMessage{
+			Destination: dest,
+			Message:     line,
+		}
+	}
+	return reply
+}
+
+func (b *EventBot) ProcessChannelEvent(msg InboundMessage) ([]OutboundMessage, error) {
+	b.NumMessagesHandled += 1
+	// !event help command
+	if strings.HasPrefix(msg.Message, fmt.Sprintf("%s %s",
+		EVENTBOT_PREFIX, EVENTBOT_CMD_HELP)) {
+		answer := strings.Split(EVENTBOT_HELP_TEXT, "\n")
+		return b.strings2reply(msg.Channel, answer), nil
+	} else if strings.HasPrefix(msg.Message, EVENTBOT_PREFIX) { // invalid command (!event foo)
+		answer := []string{EVENTBOT_INVALID_COMMAND}
+		return b.strings2reply(msg.Channel, answer), nil
+	} else { // something else
+		return nil, nil
+	}
+}
+
+func (b *EventBot) ProcessQueryEvent(msg InboundMessage) ([]OutboundMessage, error) {
+	b.NumMessagesHandled += 1
+	reply := make([]OutboundMessage, 1)
+	reply[0] = OutboundMessage{Destination: msg.Channel, Message: "Not implemented"}
+	return reply, nil
+}
+
+func (b *EventBot) GetName() string {
+	return b.BotName
+}
---
+++ src/github.com/gonium/her0ld/bots/eventbot_test.go
@@ -0,0 +1,108 @@
+package her0ldbot
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+// Ignore ordinary chat messages
+func TestNoCommand(t *testing.T) {
+	bot := NewEventBot("EventBot")
+	msg := InboundMessage{
+		Channel: "Channel",
+		Nick:    "Nick",
+		Message: "ordinarychatline",
+	}
+	response, err := bot.ProcessChannelEvent(msg)
+	if err != nil {
+		t.Fatalf("Invalid command triggered unexpected error: %s",
+			err.Error())
+	} else {
+		if len(response) != 0 {
+			t.Fatalf("Invalid response length - expected %d, got %d", 0,
+				len(response))
+		}
+	}
+}
+
+// Ignore an invalid command (i.e. for another bot)
+func TestInvalidCommand(t *testing.T) {
+	bot := NewEventBot("EventBot")
+	msg := InboundMessage{
+		Channel: "Channel",
+		Nick:    "Nick",
+		Message: "!invalidcmd",
+	}
+	response, err := bot.ProcessChannelEvent(msg)
+	if err != nil {
+		t.Fatalf("Invalid command triggered unexpected error: %s",
+			err.Error())
+	} else {
+		if len(response) != 0 {
+			t.Fatalf("Invalid response length - expected %d, got %d", 0,
+				len(response))
+		}
+	}
+}
+
+func TestEmptyCommand(t *testing.T) {
+	bot := NewEventBot("EventBot")
+	msg := InboundMessage{
+		Channel: "Channel",
+		Nick:    "Nick",
+		Message: EVENTBOT_PREFIX,
+	}
+	response, err := bot.ProcessChannelEvent(msg)
+	if err != nil {
+		t.Fatalf("Invalid command triggered unexpected error: %s",
+			err.Error())
+	} else {
+		if len(response) != 1 {
+			t.Fatalf("Invalid response length - expected %d, got %d", 1,
+				len(response))
+		} else {
+			if response[0].Destination != msg.Channel {
+				t.Fatalf("Invalid destination: Expected >%s<, got >%s<", msg.Channel,
+					response[0].Destination)
+			}
+			if response[0].Message != EVENTBOT_INVALID_COMMAND {
+				t.Fatalf("Invalid response: Expected >%s<, got >%s<",
+					EVENTBOT_INVALID_COMMAND, response[0].Message)
+			}
+		}
+	}
+}
+
+func TestHelpCommand(t *testing.T) {
+	bot := NewEventBot("EventBot")
+	line := fmt.Sprintf("%s %s", EVENTBOT_PREFIX, EVENTBOT_CMD_HELP)
+	msg := InboundMessage{
+		Channel: "Channel",
+		Nick:    "Nick",
+		Message: line,
+	}
+	response, err := bot.ProcessChannelEvent(msg)
+	if err != nil {
+		t.Fatalf("Invalid command triggered unexpected error: %s",
+			err.Error())
+	} else {
+		expected := strings.Split(EVENTBOT_HELP_TEXT, "\n")
+		expected_len := len(expected)
+		if len(response) != expected_len {
+			t.Fatalf("Invalid length of response %#v - expected %d, got %d",
+				response, expected_len, len(response))
+		} else {
+			for idx, expected_line := range expected {
+				if response[idx].Destination != msg.Channel {
+					t.Fatalf("Invalid destination: Expected >%s<, got >%s<", msg.Channel,
+						response[idx].Destination)
+				}
+				if response[idx].Message != expected_line {
+					t.Fatalf("Invalid response: Expected >%s<, got >%s<",
+						expected_line, response[idx].Message)
+				}
+			}
+		}
+	}
+}
